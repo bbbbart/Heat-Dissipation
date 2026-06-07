2d443a05299d6a3590cb5b0ca2e031cff92be5ef
bdabkowski1 <bdabkowski353@gmail.com>
2026-06-06 22:43:15 -0500

    Create updateGrid

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+diffusion_rate = 0.1
 grid = np.zeros((50, 50), dtype= int)
 
 grid[25,25] = 1
+
+def updateGrid(grid):
+    for y, row in enumerate(grid):
+        for x, element in enumerate(row):
+            delta_temp = 0
+            if y != 0:
+                delta_temp += element + diffusion_rate * (grid[y - 1, x] - element)
+            if y != 49:
+                delta_temp += element + diffusion_rate * (grid[y + 1, x] - element)
+            if x != 0:
+                delta_temp += element + diffusion_rate * (grid[y, x - 1] - element)
+            if y != 49:
+                delta_temp += element + diffusion_rate * (grid[y, x + 1] - element)
+            grid[y,x] += delta_temp
+    return grid
 
 plt.imshow(grid, cmap='viridis', interpolation='nearest')
 plt.colorbar()
