e97a5459b0c745769c899a3a864d019cfadd3216
bdabkowski1 <bdabkowski353@gmail.com>
2026-06-07 23:21:48 -0500

    Create animation with matplotlib

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots()
 
 diffusion_rate = 0.1
-grid = np.zeros((50, 50), dtype= int)
+grid = np.zeros(25, 25)
+grid[12,12] = 500
+grid[13,12] = 500
+grid[11,12] = 500
+grid[12,13] = 500
+grid[12,11] = 500
+grid[11,11] = 500
+grid[13,13] = 500
+grid[13,11] = 500
+grid[11,13] = 500
 
-grid[25,25] = 1
+image = ax.imshow(grid, vmin = 0, vmax = 500, cmap='magma', interpolation = 'bilinear')
 
-def updateGrid(grid):
+def updateGrid(frame):
+    global grid
+    grid_copy = np.copy(grid)
     for y, row in enumerate(grid):
         for x, element in enumerate(row):
-            delta_temp = 0
+            total_temp = 0
             if y != 0:
-                delta_temp += element + diffusion_rate * (grid[y - 1, x] - element)
-            if y != 49:
-                delta_temp += element + diffusion_rate * (grid[y + 1, x] - element)
+                total_temp += grid[y - 1, x]
+            if y != len(row) - 1:
+                total_temp += grid[y + 1, x]
             if x != 0:
-                delta_temp += element + diffusion_rate * (grid[y, x - 1] - element)
-            if y != 49:
-                delta_temp += element + diffusion_rate * (grid[y, x + 1] - element)
-            grid[y,x] += delta_temp
-    return grid
+                total_temp += grid[y, x - 1]
+            if x != len(row) - 1:
+                total_temp += grid[y, x + 1]
+            grid_copy[y,x] = element + diffusion_rate * (total_temp - (4 * element))
+    grid = np.copy(grid_copy)
+    image.set_data(grid)
+    return(image,)
+    
 
-plt.imshow(grid, cmap='viridis', interpolation='nearest')
-plt.colorbar()
+ani = animation.FuncAnimation(fig, updateGrid, frames = 100, interval = 100)
 plt.show()
