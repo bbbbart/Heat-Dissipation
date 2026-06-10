0fef693086c077737cb94dc3cc6a9a177f607ccd
bdabkowski1 <bdabkowski353@gmail.com>
2026-06-10 16:39:23 -0500

    Optimize updateGrid using NumPy methods

--- main.py
+++ main.py
@@ -5,38 +5,36 @@ import matplotlib.animation as animation
 fig, ax = plt.subplots()
 
 diffusion_rate = 0.1
-grid = np.zeros(25, 25)
+grid = np.zeros((25, 25))
 grid[12,12] = 500
-grid[13,12] = 500
-grid[11,12] = 500
-grid[12,13] = 500
-grid[12,11] = 500
-grid[11,11] = 500
-grid[13,13] = 500
-grid[13,11] = 500
-grid[11,13] = 500
+grid[13,12] = 250
+grid[11,12] = 250
+grid[12,13] = 250
+grid[12,11] = 250
+grid[11,11] = 250
+grid[13,13] = 250
+grid[13,11] = 250
+grid[11,13] = 250
+
 
 image = ax.imshow(grid, vmin = 0, vmax = 500, cmap='magma', interpolation = 'bilinear')
+scale = fig.colorbar(image)
 
 def updateGrid(frame):
-    global grid
-    grid_copy = np.copy(grid)
-    for y, row in enumerate(grid):
-        for x, element in enumerate(row):
-            total_temp = 0
-            if y != 0:
-                total_temp += grid[y - 1, x]
-            if y != len(row) - 1:
-                total_temp += grid[y + 1, x]
-            if x != 0:
-                total_temp += grid[y, x - 1]
-            if x != len(row) - 1:
-                total_temp += grid[y, x + 1]
-            grid_copy[y,x] = element + diffusion_rate * (total_temp - (4 * element))
-    grid = np.copy(grid_copy)
-    image.set_data(grid)
-    return(image,)
-    
+    center = grid[1:-1, 1:-1]
+    up = grid[:-2, 1:-1]
+    down = grid[2:, 1:-1]
+    left = grid[1:-1, :-2]
+    right = grid[1:-1, 2:]
 
+    total_heat = np.sum(grid)
+
+    heat_update = np.pad(center + diffusion_rate * ((up + down + left + right) - 4 * center), 1, mode = 'constant') 
+    grid = heat_update
+    
+    image.set_data(grid)
+    ax.set_title(f"Total heat: {total_heat}")
+    return(image)
+    
 ani = animation.FuncAnimation(fig, updateGrid, frames = 100, interval = 100)
 plt.show()
