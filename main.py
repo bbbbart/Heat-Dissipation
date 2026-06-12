1257456733a62b6b01facbb59e4dd437f7664047
bdabkowski1 <bdabkowski353@gmail.com>
2026-06-12 17:39:57 -0500

    Bug fixes and create ability to reset grid

--- main.py
+++ main.py
@@ -4,20 +4,19 @@ import matplotlib.animation as animation
 
 fig, ax = plt.subplots()
 
-diffusion_rate = 0.05
+diffusion_rate = 0.1
 grid = np.zeros((25, 25))
 ghost_grid = np.zeros((27,27))
 
 pause = False
-grid[12,12] = 1500
 
-image = ax.imshow(grid, vmin = 0, vmax = 2000, cmap='magma', interpolation = 'bilinear')
+image = ax.imshow(grid, vmin = 0, vmax = 500, cmap='magma', interpolation = 'bilinear')
 scale = fig.colorbar(image)
 
 def addHeat(event):
     if event.inaxes is not None:
-       grid[int(event.ydata), int(event.xdata)] = 1500
-       image.set_data(grid)
+      grid[int(event.ydata), int(event.xdata)] += 500
+    image.set_data(grid)
 
 def buttonPresses(event):
     global pause
@@ -26,6 +25,12 @@ def buttonPresses(event):
         pause = False
       else:
         pause = True
+    if event.key == 'r':
+       resetGrid()
+       
+def resetGrid():
+   grid.fill(0)
+   ghost_grid.fill(0)
 
 fig.canvas.mpl_connect('key_press_event', buttonPresses)
 fig.canvas.mpl_connect('button_press_event', addHeat)
@@ -34,8 +39,8 @@ def updateGrid(frame):
     while not pause:
       global grid
       ghost_grid[1:-1, 1:-1] = grid ## makes center of ghost grid equal to grid
-      ghost_grid[0::len(grid[0]), 1:-1] = grid[0::len(grid[0])]   ## makes top and bottom row of grid equal to ghost grid
-      ghost_grid[1:-1, 0::len(grid[0])] = grid[:, 0::len(grid[0])] ## makes left and right row of grid equal to ghost grid
+      ghost_grid[0::len(ghost_grid)-1, 1:-1] =  grid[0::len(grid)-1]  ## makes top and bottom row of grid equal to ghost grid
+      ghost_grid[1:-1, 0::len(ghost_grid)-1] = grid[:, 0::len(grid)-1] ## makes left and right row of grid equal to ghost grid
 
       up = ghost_grid[:-2, 1:-1]
       down = ghost_grid[2:, 1:-1]
@@ -44,8 +49,7 @@ def updateGrid(frame):
 
       total_heat = np.sum(grid)
 
-      ghost_grid[1:-1, 1:-1] = grid + diffusion_rate * ((up + down + left + right) - 4 * grid) ## updates center of ghost grid while also using border cells for calculations
-      grid = ghost_grid[1:-1, 1:-1] ## updates grid
+      grid = grid + diffusion_rate * ((up + down + left + right) - 4 * grid) ## updates center of ghost grid while also using border cells for calculations
 
       image.set_data(grid)
       ax.set_title(f"Total heat: {total_heat}")
@@ -55,5 +59,5 @@ def updateGrid(frame):
       ax.set_title("Paused")
       return(image)
 
-ani = animation.FuncAnimation(fig, updateGrid, frames = 100, interval = 100)
+ani = animation.FuncAnimation(fig, updateGrid, frames = 999, interval = 200)
 plt.show()
