35911b7911912ad5cac287ee5700d9922cc5c1dd
bdabkowski1 <bdabkowski353@gmail.com>
2026-06-12 00:10:23 -0500

    Create pausing and adding heat sources

--- main.py
+++ main.py
@@ -4,43 +4,56 @@ import matplotlib.animation as animation
 
 fig, ax = plt.subplots()
 
-diffusion_rate = 0.1
+diffusion_rate = 0.05
 grid = np.zeros((25, 25))
 ghost_grid = np.zeros((27,27))
-grid[12,12] = 500
-grid[13,12] = 250
-grid[11,12] = 250
-grid[12,13] = 250
-grid[12,11] = 250
-grid[11,11] = 250
-grid[13,13] = 250
-grid[13,11] = 250
-grid[11,13] = 250
 
+pause = False
+grid[12,12] = 1500
 
-image = ax.imshow(grid, vmin = 0, vmax = 500, cmap='magma', interpolation = 'bilinear')
+image = ax.imshow(grid, vmin = 0, vmax = 2000, cmap='magma', interpolation = 'bilinear')
 scale = fig.colorbar(image)
 
+def addHeat(event):
+    if event.inaxes is not None:
+       grid[int(event.ydata), int(event.xdata)] = 1500
+       image.set_data(grid)
+
+def buttonPresses(event):
+    global pause
+    if event.key ==  ' ':
+      if pause:
+        pause = False
+      else:
+        pause = True
+
+fig.canvas.mpl_connect('key_press_event', buttonPresses)
+fig.canvas.mpl_connect('button_press_event', addHeat)
+
 def updateGrid(frame):
-    global grid
+    while not pause:
+      global grid
+      ghost_grid[1:-1, 1:-1] = grid ## makes center of ghost grid equal to grid
+      ghost_grid[0::len(grid[0]), 1:-1] = grid[0::len(grid[0])]   ## makes top and bottom row of grid equal to ghost grid
+      ghost_grid[1:-1, 0::len(grid[0])] = grid[:, 0::len(grid[0])] ## makes left and right row of grid equal to ghost grid
 
-    ghost_grid[1:-1, 1:-1] = grid
-    ghost_grid[0::len(grid[0]), 1:-1] = grid[0::len(grid[0])]
-    ghost_grid[1:-1, 0::len(grid[0])] = grid[:, 0::len(grid[0])]
+      up = ghost_grid[:-2, 1:-1]
+      down = ghost_grid[2:, 1:-1]
+      left = ghost_grid[1:-1, :-2]
+      right = ghost_grid[1:-1, 2:]
 
-    up = ghost_grid[:-2, 1:-1]
-    down = ghost_grid[2:, 1:-1]
-    left = ghost_grid[1:-1, :-2]
-    right = ghost_grid[1:-1, 2:]
+      total_heat = np.sum(grid)
 
-    total_heat = np.sum(grid)
+      ghost_grid[1:-1, 1:-1] = grid + diffusion_rate * ((up + down + left + right) - 4 * grid) ## updates center of ghost grid while also using border cells for calculations
+      grid = ghost_grid[1:-1, 1:-1] ## updates grid
 
-    ghost_grid[1:-1, 1:-1] = grid + diffusion_rate * ((up + down + left + right) - 4 * grid)
-    grid = ghost_grid[1:-1, 1:-1]
+      image.set_data(grid)
+      ax.set_title(f"Total heat: {total_heat}")
+      return(image)
+  
+    else:
+      ax.set_title("Paused")
+      return(image)
 
-    image.set_data(grid)
-    ax.set_title(f"Total heat: {total_heat}")
-    return(image)
-    
 ani = animation.FuncAnimation(fig, updateGrid, frames = 100, interval = 100)
 plt.show()
